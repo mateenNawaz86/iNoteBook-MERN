9ebbc8c748fd9a03a10aa580cafcb71b5a57699f
Mateen Mirani <mateenmirani125@gmail.com>
2021-09-11 17:36:20 +0500

    create component & Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 const App = () => {
   return (
     <>
-      <h1>Hey! First MERN APP!</h1>
+      <Router>
+        <Navbar />
+        <div className="container my-5">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 };
